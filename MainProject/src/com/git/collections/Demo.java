62776a50aff9221534cf536c6fe06823043ebbc3
Prasad <prasadkalmegh123@gmail.com>
2023-07-14 02:45:23 +0530

    Added Arraylist code

---
+++ MainProject/src/com/git/collections/Demo.java
@@ -0,0 +1,16 @@
+package com.git.collections;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	 ArrayList al =new ArrayList();
+	 al.add(26);
+	 al.add("golu");
+	 al.add(null);
+	 al.add(null);
+	 System.out.println(al);
+	}
+
+}
